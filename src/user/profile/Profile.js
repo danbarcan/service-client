a5e86b36fce1cc88754808a4f8b38828b9e9c709
Dan Popouţanu <dan@princh.com>
2019-09-05 23:35:12 +0100

    fixat validarea cu toate campurile la edit user

--- src/user/profile/Profile.js
+++ src/user/profile/Profile.js
@@ -2,7 +2,17 @@ import React, { Component } from "react";
 import { updateUser } from "../../util/APIUtils";
 import { Redirect } from 'react-router';
 import { Form, Input, Button, Icon, notification } from "antd";
-
+import {
+  NAME_MIN_LENGTH,
+  NAME_MAX_LENGTH,
+  USERNAME_MIN_LENGTH,
+  USERNAME_MAX_LENGTH,
+  EMAIL_MAX_LENGTH,
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_MAX_LENGTH,
+  PHONE_MAX_LENGTH,
+  PHONE_MIN_LENGTH
+} from "../../constants";
 import "./Profile.css";
 const FormItem = Form.Item;
 
@@ -11,69 +21,72 @@ export class Profile extends Component {
     super(props);
 
     this.state = {
-      name: "",
-      pw: " ",
-      newpw: " ",
-      phone: " ",
-      email: "",
-      society: "",
-      address: "",
+      name: {
+        value: ""
+      },
+      pw: {
+        value: ""
+      },
+      newpw: {
+        value: ""
+      },
+      phone: {
+        value: ""
+      },
+      email: {
+        value: ""
+      },
+      society: {
+        value: ""
+      },
+      address: {
+        value: ""
+      },
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.validate = this.validate.bind(this);
     this.handleNewPassword = this.handleNewPassword.bind(this);
-  }
-  validate(values) {
-    const errors = {};
-    if (!values.name || values.title.trim() === "") {
-      errors.title = "‘Enter a Title’";
-    }
-    if (!values.categories || values.categories.trim() === "") {
-      errors.categories = "‘Enter categories’";
-    }
-    if (!values.content || values.content.trim() === "") {
-      errors.content = "‘Enter some content’";
-    }
-    return errors;
+    this.isFormInvalid = this.isFormInvalid.bind(this);
   }
 
-
-
-  handleChange(event) {
+  handleChange(event, validationFun) {
     const target = event.target;
     const inputName = target.name;
     const inputValue = target.value;
-    this.validate(inputValue);
 
     this.setState({
-      [inputName]: inputValue
+      [inputName]: {
+        value: inputValue,
+        ...validationFun(inputValue)
+      }
     });
   }
 
   handleNewPassword(event) {
-    if (event.target.value === this.state.pw) {
-      notification.warning({
-        message: "Polling App",
-        description:
-          " Parola trebuie sa fie diferita. Va rugam reincercati",
-        duration: 10
-      });
-    } else {
-      const target = event.target;
-      const inputName = target.name;
-      const inputValue = target.value;
-      this.validate(inputValue);
+    console.log('hei');
+    // if (event.target.value === this.state.pw) {
+    //   notification.warning({
+    //     message: "Polling App",
+    //     description:
+    //       " Parola trebuie sa fie diferita. Va rugam reincercati",
+    //     duration: 10
+    //   });
+    // } else {
+    //   const target = event.target;
+    //   const inputName = target.name;
+    //   const inputValue = target.value;
+    //   this.validate(inputValue);
 
-      this.setState({
-        [inputName]: inputValue
-      });
-    }
+    //   this.setState({
+    //     [inputName]: inputValue
+    //   });
+    // }
   }
 
   handleSubmit(event) {
     event.preventDefault();
+
     if (this.state.newpw === this.state.pw) {
       this.setState({
         incorrectPassword: true
@@ -81,14 +94,14 @@ export class Profile extends Component {
     } else {
       const userRequest = {
         id: this.props.currentUser.id,
-        name: this.state.name,
+        name: this.state.name.value,
         username: this.props.currentUser.username,
-        oldPassword: this.state.pw,
-        password: this.state.newpw,
-        phone: this.state.phone,
-        email: this.state.email,
-        serviceName: this.state.society,
-        serviceAddress: this.state.address,
+        pw: this.state.pw.value,
+        newpw: this.state.newpw.value,
+        phone: this.state.phone.value,
+        email: this.state.email.value,
+        serviceName: this.state.society.value,
+        serviceAddress: this.state.address.value,
         redirectHome: false
       };
       updateUser(userRequest)
@@ -105,11 +118,35 @@ export class Profile extends Component {
           notification.error({
             message: "Polling App",
             description:
-              "Oups! Ceva nu a mers corect, va rugam reincercati!"
+              "Oups! Va rugam sa introduceti parola actuala corecta!"
           });
         });
     }
   }
+
+  isFormInvalid() {
+    console.log(this.props);
+    return !(
+      this.state.name.validateStatus === "success" &&
+      this.state.pw.validateStatus === "success" &&
+      this.state.newpw.validateStatus === "success" &&
+      this.state.phone.validateStatus === "success" &&
+      this.state.email.validateStatus === "success"
+    );
+  }
+
+  // isFormInvalidStaff() {
+  //   return !(
+  //     this.state.name.validateStatus === "success" &&
+  //     this.state.username.validateStatus === "success" &&
+  //     this.state.email.validateStatus === "success" &&
+  //     this.state.phone.validateStatus === "success" &&
+  //     this.state.password.validateStatus === "success" &&
+  //     this.state.service_name.validateStatus === "success" &&
+  //     this.state.service_address.validateStatus === "success" &&
+  //     this.state.cui.validateStatus === "success"
+  //   );
+  // }
 
   render() {
 
@@ -123,55 +160,71 @@ export class Profile extends Component {
           <h2> Profil </h2>
           <h3> Nume utilizator : {this.props.currentUser.username}</h3>
           <Form onSubmit={this.handleSubmit} className="profile-form">
-            <FormItem>
+            <FormItem
+              label="Nume"
+              validateStatus={this.state.name.validateStatus}
+              help={this.state.name.errorMsg}>
               <Input
                 prefix={<Icon type="profile" />}
                 size="large"
                 name="name"
                 type="text"
+                value={this.state.name.value}
                 placeholder="Nume de utilizator"
-                onChange={event => this.handleChange(event)}
+                onChange={event => this.handleChange(event, this.validateName)}
               />
             </FormItem>
-            <FormItem>
+            <FormItem label="Parola curenta"
+              validateStatus={this.state.pw.validateStatus}
+              help={this.state.pw.errorMsg}>
               <Input
                 prefix={<Icon type="lock" />}
                 size="large"
                 name="pw"
                 type="password"
+                value={this.state.pw.value}
                 required
                 placeholder="Parola curenta"
-                onChange={event => this.handleChange(event)}
+                onChange={event => this.handleChange(event, this.validatePw)}
               />
             </FormItem>
-            <FormItem>
+            <FormItem label=" Parola noua"
+              validateStatus={this.state.newpw.validateStatus}
+              help={this.state.newpw.errorMsg}>
               <Input
                 prefix={<Icon type="lock" />}
                 size="large"
                 name="newpw"
                 type="password"
+                value={this.state.newpw.value}
                 placeholder="Parola noua"
-                onChange={event => this.handleNewPassword(event)}
+                onChange={event => this.handleChange(event, this.validateNewPw)}
               />
             </FormItem>
-            <FormItem>
+            <FormItem label=" Numar de telefon"
+              validateStatus={this.state.phone.validateStatus}
+              help={this.state.phone.errorMsg}>
               <Input
                 prefix={<Icon type="phone" />}
                 size="large"
                 name="phone"
                 type="number"
+                value={this.state.phone.value}
                 placeholder="Numar de telefon"
-                onChange={event => this.handleChange(event)}
+                onChange={event => this.handleChange(event, this.validatePhone)}
               />
             </FormItem>
-            <FormItem>
+            <FormItem label="Adresa de email"
+              validateStatus={this.state.email.validateStatus}
+              help={this.state.email.errorMsg}>
               <Input
                 prefix={<Icon type="mail" />}
                 size="large"
                 name="email"
                 type="email"
+                value={this.state.email.value}
                 placeholder="Adresa de email"
-                onChange={event => this.handleChange(event)}
+                onChange={event => this.handleChange(event, this.validateEmail)}
               />
             </FormItem>
             <Button
@@ -179,6 +232,7 @@ export class Profile extends Component {
               htmlType="submit"
               size="large"
               className="profile-form-button"
+              disabled={this.isFormInvalid()}
             >
               Salvare
             </Button>
@@ -192,57 +246,76 @@ export class Profile extends Component {
           <h3> Nume utilizator : {this.props.currentUser.username}</h3>
 
           <Form onSubmit={this.handleSubmit} className="profile-form">
-            <FormItem>
+            <FormItem
+              validateStatus={this.state.pw.validateStatus}
+              help={this.state.pw.errorMsg}>
               <Input
                 prefix={<Icon type="lock" />}
                 size="large"
                 name="pw"
                 type="password"
+                value={this.state.pw.value}
                 placeholder="Parola curenta"
               />
             </FormItem>
-            <FormItem>
+            <FormItem
+              validateStatus={this.state.newpw.validateStatus}
+              help={this.state.newpw.errorMsg}>
               <Input
                 prefix={<Icon type="lock" />}
                 size="large"
                 name="newpw"
                 type="password"
+                value={this.state.newpw.value}
                 placeholder="Parola noua"
               />
             </FormItem>
-            <FormItem>
+            <FormItem
+              validateStatus={this.state.phone.validateStatus}
+              help={this.state.phone.errorMsg}>
               <Input
                 prefix={<Icon type="phone" />}
                 size="large"
                 name="phone"
                 type="number"
+                value={this.state.phone.value}
                 placeholder="Numar de telefon"
               />
             </FormItem>
-            <FormItem>
+            <FormItem
+              validateStatus={this.state.email.validateStatus}
+              help={this.state.email.errorMsg}>
               <Input
                 prefix={<Icon type="mail" />}
                 size="large"
                 name="email"
                 type="email"
+                value={this.state.email.value}
                 placeholder="Adresa de email"
               />
             </FormItem>
-            <FormItem>
+            <FormItem
+              validateStatus={this.state.society.validateStatus}
+              help={this.state.society.errorMsg}>
               <Input
                 prefix={<Icon type="user" />}
                 size="large"
                 name="society"
                 type="text"
+                value={this.state.society.value}
                 placeholder="Nume firma"
               />
             </FormItem>
-            <FormItem>
+            <FormItem
+              validateStatus={this.state.address.validateStatus}
+              help={this.state.address.errorMsg}>
               <Input
                 prefix={<Icon type="search" />}
                 size="large"
                 name="address"
                 type="text"
+                value={this.state.address.value}
+
                 placeholder="Adresa firma"
               />
             </FormItem>
@@ -259,7 +332,120 @@ export class Profile extends Component {
       );
     }
   }
+  validateName = name => {
+    if (name.length < NAME_MIN_LENGTH) {
+      return {
+        validateStatus: "error",
+        errorMsg: `Numele este prea scurt (Minim ${NAME_MIN_LENGTH} caractere necesare.)`
+      };
+    } else if (name.length > NAME_MAX_LENGTH) {
+      return {
+        validateStatus: "error",
+        errorMsg: `Numele este prea lung (Maxim ${NAME_MAX_LENGTH} caractere.)`
+      };
+    } else {
+      return {
+        validateStatus: "success",
+        errorMsg: null
+      };
+    }
+  };
+
+  validatePhone = phone => {
+    if (phone.length < PHONE_MIN_LENGTH) {
+      return {
+        validateStatus: "error",
+        errorMsg: `Numarul de telefon este prea scurt.`
+      };
+    } else if (phone.length > PHONE_MAX_LENGTH) {
+      return {
+        validateStatus: "error",
+        errorMsg: `Numarul de telefon este prea lung.`
+      };
+    } else {
+      return {
+        validateStatus: "success",
+        errorMsg: null
+      };
+    }
+  };
+
+  validateEmail = email => {
+    if (!email) {
+      return {
+        validateStatus: "error",
+        errorMsg: "Emailul este obligatoriu"
+      };
+    }
+
+    const EMAIL_REGEX = RegExp("[^@ ]+@[^@ ]+\\.[^@ ]+");
+    if (!EMAIL_REGEX.test(email)) {
+      return {
+        validateStatus: "error",
+        errorMsg: "Email nu este valid"
+      };
+    }
+
+    if (email.length > EMAIL_MAX_LENGTH) {
+      return {
+        validateStatus: "error",
+        errorMsg: `Emailul este prea lung`
+      };
+    }
+
+    return {
+      validateStatus: 'success',
+      errorMsg: null
+    };
+  };
+
+  validatePw = pw => {
+    if (!pw) {
+      return {
+        validateStatus: "error",
+        errorMsg: "Parola actuala este obligatorie"
+      };
+    }
+    if (pw.length < PASSWORD_MIN_LENGTH) {
+      return {
+        validateStatus: "error",
+        errorMsg: `Parola este prea scurta`
+      };
+    }
+    return {
+      validateStatus: 'success',
+      errorMsg: null
+    };
+  }
+
+
+  validateNewPw = newpw => {
+    if (!newpw) {
+      return {
+        validateStatus: "error",
+        errorMsg: "Parola noua este obligatorie"
+      };
+    }
+    if (newpw === this.state.pw.value) {
+      return {
+        validateStatus: "error",
+        errorMsg: "Parola noua trebuie sa fie diferita de parola actuala"
+      };
+    }
+    if (newpw.length < PASSWORD_MIN_LENGTH) {
+      return {
+        validateStatus: "error",
+        errorMsg: `Parola este prea scurta`
+      };
+    }
+    return {
+      validateStatus: 'success',
+      errorMsg: null
+    };
+  }
 }
+
+
 
 
 export default Profile;
